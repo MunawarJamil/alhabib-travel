4180f7f5c323f0b22a97e861926f3127bb94558b
Munawar Jamil <munawar.minhas777@gmail.com>
2024-12-03 00:31:07 +0500

    improved code quality

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -2,36 +2,38 @@
 
 import Image from "next/image";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { useState } from "react";
 import Carousel from "./Carousel";
 import Footer from "./Footer";
-import { useState } from "react";
-import { imagesData } from "../data/images";
 import TravelDetails from "./TravelDetails";
 import About from "./About";
+import { imagesData } from "../data/images";
 
 // Reusable Dropdown Button Component
-const DropdownButton = ({ label, backgroundColor }) => (
-  <button className={`flex items-center ${backgroundColor} text-white py-2 px-5 md:px-6`}>
+const DropdownButton = ({ label, backgroundColor, isActive, onClick }) => (
+  <button
+    onClick={onClick} // Attach the onClick handler
+    className={`flex items-center ${backgroundColor} text-white py-2 lg:py-4  px-5 md:px-6 rounded-md transition hover:opacity-90`}
+  >
     <span>{label}</span>
-    <IoMdArrowDropdown />
+    <IoMdArrowDropdown className="ml-2" />
   </button>
 );
 
 function HomePage() {
-
-  const [activeTab, setActiveTab] = useState("makkah"); 
-
-
-  const [selectedImage, setSelectedImage] = useState("/desktopImages/Snood-Big-Hotel.webp");
+  const [activeTab, setActiveTab] = useState("makkah"); // State to manage active tab
+  const [selectedImage, setSelectedImage] = useState(
+    "/desktopImages/Snood-Big-Hotel.webp"
+  );
 
   return (
     <>
       {/* Hero Section */}
       <main>
         <Image
-          src="/IMG-20241130-WA0001.jpg"
+          src="/banner-home.jpg"
           alt="banner image"
-          className="w-full max-h-[90vh]"
+          className="w-full max-h-[100vh] object-cover "
           width={1920}
           height={1080}
           priority
@@ -39,93 +41,206 @@ function HomePage() {
       </main>
 
       <section className="md:w-[70%] mx-auto md:mt-32">
-      <div className="p-5">
-        <p className="text-[#d4A10F] text-2xl">Your Comfort, Our Priority</p>
-        <h1 className="text-4xl mt-2 font-bold">Hotels / Accommodations</h1>
-      </div>
-
-      <div className="px-5 py-1">
-        <div className="makkah flex gap-1">
-          <DropdownButton
-            label="Makkah"
-            backgroundColor="bg-[#00454A]"
-            onClick={() => setActiveTab("makkah")}
-          />
-          <DropdownButton
-            label="Madina"
-            backgroundColor="bg-[#D4A10F]"
-            onClick={() => setActiveTab("madina")}
-          />
-          <DropdownButton
-            label="Food"
-            backgroundColor="bg-[#D4A10F]"
-            onClick={() => setActiveTab("food")}
-          />
-        </div>
-      </div>
-
-      {/* Conditionally Render Makkah Hotels */}
-      {activeTab === "makkah" && (
-        <div className="p-5">
-          {/* Makkah Hotel Content */}
-          <div>
-            <span className="font-bold text-lg text-[#00454A] ">Emaar Al Khalil Makkah</span>
-            <div className="py-2 text-gray-500">
-              <p>
-                Emaar Al Khalil is ideally located on Ibrahim Khalil Street, just
-                400 meters from Haram Sharif...
-              </p>
-              <p>
-                <span className="font-bold text-[#d4A10F] ">Location :</span>
-                <span>Ibrahim Al Khalil Street - Mesfala, 34452 Makkah, Saudi Arabia</span>
-              </p>
-            </div>
-          </div>
-          {/* More Makkah Hotels */}
-        </div>
-      )}
-
-      {/* Conditionally Render Madina Hotels */}
-      {activeTab === "madina" && (
-        <div className="p-5">
-          {/* Madina Hotel Content */}
-          <div>
-            <span className="font-bold text-lg text-[#00454A] ">Odst Al Madinah</span>
-            <div className="py-2 text-gray-500">
-              <p>
-                Odst Al Madinah is just 150 meters from the Prophet’s Mosque...
-              </p>
-              <p>
-                <span className="font-bold text-[#d4A10F] ">Location :</span>
-                <span>Golden Tulip Al Mektan Madinah Hotel</span>
-              </p>
-            </div>
-          </div>
-          {/* More Madina Hotels */}
-        </div>
-      )}
-
-      {/* Conditionally Render Food Section */}
-      {activeTab === "food" && (
-        <div className="p-5">
-          {/* Food Content */}
-          <span className="font-bold text-lg text-[#00454A] ">FOOD</span>
-          <div className="py-2 text-gray-500">
-            <p>
-              At Al Habib Travel Ltd, we cater to the diverse tastes of Pakistani, Indian...
+        <div>
+          <div className="p-5">
+            <p className="text-[#d4A10F] text-2xl">
+              Your Comfort, Our Priority
             </p>
+            <h1 className="text-4xl mt-2 font-bold">Hotels / Accommodations</h1>
           </div>
+
+          <div className="px-5 py-1">
+            <div className="makkah flex gap-2">
+              <DropdownButton
+                label="Makkah"
+                backgroundColor="bg-[#00454A] hover:bg-[#d4A10F]"
+                onClick={() => setActiveTab("makkah")}
+              />
+              <DropdownButton
+                label="Madina"
+                backgroundColor="bg-[#00454A]  hover:bg-[#d4A10F]"
+                onClick={() => setActiveTab("madina")}
+              />
+              <DropdownButton
+                label="Food"
+                backgroundColor="bg-[#00454A]  hover:bg-[#d4A10F]"
+                onClick={() => setActiveTab("food")}
+              />
+            </div>
+          </div>
+
+          {/* Conditional Rendering for Tabs */}
+          {activeTab === "makkah" && (
+            <div className="p-5">
+              <div>
+                <span className="font-bold text-lg text-[#00454A] ">
+                  Emaar Al Khalil Makkah
+                </span>
+                <div className="py-2 text-gray-500">
+                  <p>
+                    Emaar Al Khalil is ideally located on Ibrahim Khalil Street,
+                    just 400 meters from Haram Sharif. Enjoy air-conditioned
+                    rooms with flat-screen TVs, a kettle, and 24/7 front desk
+                    service in Arabic and English. Our dedicated staff ensures a
+                    comfortable stay for all guests. Book now!
+                  </p>
+                  <p>
+                    <span className="font-bold text-[#d4A10F] ">
+                      Location :
+                    </span>
+                    <span>
+                      Ibrahim Al Khalil Street - Mesfala, 34452 Makkah, Saudi
+                      Arabia
+                    </span>
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <span className="font-bold text-lg text-[#00454A] ">
+                  Emaar Grand Makkah Hotel
+                </span>
+                <div className="py-2 text-gray-500">
+                  <p>
+                    Experience comfort at Emaar Grand Makkah Hotel, a 4-star
+                    hotel just 4-5 minutes’ walk to Haram, proudly partnered
+                    with Al Habib Travel Ltd for your Umrah. Enjoy modern rooms
+                    with stunning views of Al Haram and Makkah city.
+                  </p>
+                  <p>
+                    <span className=" text-[#d4A10F]  font-bold  ">
+                      Location :
+                    </span>
+                    <span>
+                      Ibrahim Al Khalil Street - Mesfala, 24231 Makkah, Saudi
+                      Arabia
+                    </span>
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <span className="font-bold text-lg text-[#00454A] ">
+                  Anjum Makkah
+                </span>
+                <div className="py-2 text-gray-500">
+                  <p>
+                    Anjum Makkah, just a 2-minute walk from Masjid Al Haram,
+                    offers modern accommodations with stunning Haram views.
+                    Guests can savor local and international cuisine at the
+                    on-site restaurant or enjoy 24/7 room service. With a
+                    24-hour front desk, Al Habib Travel ensures a seamless and
+                    comfortable Umrah experience.
+                  </p>
+                  <p>
+                    <span className=" text-[#d4A10F]  font-bold  ">
+                      Location :
+                    </span>
+                    <span>
+                      Umm Al Qura Street, Jabal Al Kaaba District, Makkah, Saudi
+                      Arabia
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {activeTab === "madina" && (
+            <div className="p-5">
+              <div>
+                <span className="font-bold text-lg text-[#00454A] ">
+                  Odst Al Madinah{" "}
+                </span>
+                <div className="py-2 text-gray-500">
+                  <p>
+                    Odst Al Madinah is just 150 meters from the Prophet’s Mosque
+                    and 250 meters from the Old Bazaar, offering unmatched
+                    convenience for pilgrims. Each room features a private
+                    bathroom, bathtub, slippers, free toiletries, and a
+                    hairdryer. Located 13 km from Prince Mohammed Airport, it’s
+                    the perfect Umrah stay! Location: Odst, Madinah 42311, Saudi
+                    Arabia
+                  </p>
+                  <p>
+                    <span className="font-bold text-[#d4A10F] ">
+                      Location :
+                    </span>
+                    <span>Golden Tulip Al Mektan Madinah Hotel</span>
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <span className="font-bold text-lg text-[#00454A] ">
+                  Golden Tulip Al Mektan Madinah Hotel
+                </span>
+                <div className="py-2 text-gray-500">
+                  <p>
+                    Golden Tulip Al Mektan, on Bab Al Salam Street, is just 200
+                    meters and a 2-minute walk from Haram Sharif, offering
+                    stunning views of the Prophet’s Mosque. Enjoy comfort and
+                    style at this premier Madinah hotel, proudly partnered with
+                    Al Habib Travel Ltd for your Hajj and Umrah tours.
+                  </p>
+                  <p>
+                    <span className=" text-[#d4A10F]  font-bold  ">
+                      Location :
+                    </span>
+                    <span>
+                      As Salam Rd, Al Manakhah, Madinah 42311, Saudi Arabia
+                    </span>
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <span className="font-bold text-lg text-[#00454A] ">
+                  Pullman Zam Zam Madinah
+                </span>
+                <div className="py-2 text-gray-500">
+                  <p>
+                    Pullman Zam Zam is just 100 meters from Bab Al Salam at
+                    Masjid Al Nabawi, offering unparalleled convenience. Enjoy
+                    air-conditioned rooms with free WiFi, flat-screen TVs, and
+                    select rooms with seating areas and sofa beds. With 4 unique
+                    restaurants, Al Habib Travel ensures a luxurious and
+                    comfortable Umrah stay.
+                  </p>
+                  <p>
+                    <span className=" text-[#d4A10F]  font-bold  ">
+                      Location :
+                    </span>
+                    <span>
+                      Amr Bin Al Gmoh Street, Bani Khidrah, Madinah 41499, Saudi
+                      Arabia
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {activeTab === "food" && (
+            <div className="p-5">
+              <span className="font-bold text-lg text-[#00454A] ">FOOD </span>
+              <div className="py-2 text-gray-500">
+                <p>
+                  At Al Habib Travel Ltd, we cater to the diverse tastes of
+                  Pakistani, Indian, Bangladeshi, British, and European Muslims.
+                  From flavorful biryanis and curries to familiar international
+                  dishes, our meals are crafted with care, including a special
+                  Zabiha dish every week to make your journey even more
+                  comforting.
+                </p>
+              </div>
+            </div>
+          )}
         </div>
-      )}
-
-
-
-
-
 
         {/* Mobile Images */}
         <div className="flex flex-col lg:flex-row gap-5 p-5">
-          {/* Smaller Images */}
+          {/* Thumbnail Images */}
           <div className="flex overflow-x-auto lg:flex-col md:overflow-y-auto gap-3">
             {imagesData.map((image, index) => (
               <Image
@@ -135,7 +250,7 @@ function HomePage() {
                 className="rounded-lg cursor-pointer hover:border hover:border-gray-400 transition"
                 width={100}
                 height={100}
-                onClick={() => setSelectedImage(image.large)} // Update selected image
+                onClick={() => setSelectedImage(image.large)}
               />
             ))}
           </div>
@@ -154,16 +269,14 @@ function HomePage() {
         </div>
       </section>
 
-      {/* Umrah Packages */}
+      {/* Additional Sections */}
       <section>
         <Carousel />
         <TravelDetails />
       </section>
 
-      {/* About Us Section */}
       <About />
 
-      {/* Footer Section */}
       <section>
         <Footer />
       </section>
